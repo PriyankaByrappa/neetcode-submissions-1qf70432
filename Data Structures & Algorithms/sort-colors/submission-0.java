314001028bd380f1c0fdd3b54c2ae6c5f8a24e86
PriyankaByrappa <152691118+PriyankaByrappa@users.noreply.github.com>
2026-05-04 16:19:06 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+    }
+        void print(int[] nums)
+    {
+        for(int i=0;i<nums.length;i++)
+        {
+            System.out.print(nums[i]+" ");
+        }
+        System.out.println();
+    
+    }
+}
